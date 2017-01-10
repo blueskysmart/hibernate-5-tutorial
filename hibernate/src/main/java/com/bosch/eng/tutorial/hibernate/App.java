ffe3bf8e308c6b036d770eaaf2e37a6722a937c3
Cuong Nguyen Phu <Cuong.NguyenPhu2@vn.bosch.com>
2017-01-10 15:35:06 +0700

    init project

---
+++ hibernate/src/main/java/com/bosch/eng/tutorial/hibernate/App.java
@@ -0,0 +1,13 @@
+package com.bosch.eng.tutorial.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
